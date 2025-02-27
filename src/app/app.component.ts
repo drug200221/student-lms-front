8973877add37dcc174e940a2117055d3f0d7b725
Георгий <drug200221@gmail.com>
2025-02-27 10:11:16 +0300

    подключен angular material - сменил префикс генерируемых компонентов (psk) - добавил toolbar вместо базовой страницы

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatIconButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatToolbar, MatIconButton, MatIcon],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
